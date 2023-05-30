1b387773e522904744ba305c24846e0aaa3d9975
Egyed József <jegyed50@gmail.com>
2023-05-30 08:53:05 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     sense = sense + 100
+    Showsense()
 })
+function Showsense () {
+    led.plotBarGraph(
+    Math.map(sense, -700, 700, 0, 700),
+    700
+    )
+    basic.pause(1000)
+}
 input.onButtonPressed(Button.AB, function () {
     sense = 700
+    Showsense()
 })
 input.onButtonPressed(Button.B, function () {
     sense = sense - 100
+    Showsense()
 })
 let y = 0
 let x = 0
 let sense = 0
 sense = 700
+Showsense()
 basic.forever(function () {
     basic.clearScreen()
     if (x < -700) {
